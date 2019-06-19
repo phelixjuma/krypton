e725282b815b6be2df9f312425ec6f9738ae9d09
Juma Phelix <jumaphelix@gmail.com>
2019-06-19 09:20:59 +0300

    clearing tracking

--- tests/LoadEnv.php
+++ tests/LoadEnv.php
@@ -22,26 +22,26 @@ $_ENV['DB_USER']     =   "root";
 $_ENV['DB_PASSWORD'] =   "pass";
 
 // Mail server configurations
-$_ENV['MAIL_HOST']       = "rav4.websitewelcome.com";
-$_ENV['MAIL_SMTPAUTH']   = "true";
-$_ENV['MAIL_USERNAME']   = "developers@kuzalab.com";
-$_ENV['MAIL_PASSWORD']   = "Developer.KuzaLab@18";
-$_ENV['MAIL_SMTPSECURE'] = "tls";
-$_ENV['MAIL_PORT']       = "587";
+$_ENV['MAIL_HOST']       = "";
+$_ENV['MAIL_SMTPAUTH']   = "";
+$_ENV['MAIL_USERNAME']   = "";
+$_ENV['MAIL_PASSWORD']   = "";
+$_ENV['MAIL_SMTPSECURE'] = "";
+$_ENV['MAIL_PORT']       = "";
 
 // AWS Configs
-$_ENV['AWS_ACCESS_KEY']              = "AKIAJHXZKYMCFTQVIDQQ";
-$_ENV['AWS_SECRET_KEY']              = "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";
-$_ENV['S3_BUCKET_NAME']              =   "gmoney-public-uploads";
-$_ENV['CLOUDFRONT_URL']              =   "https://deq3lhx6sylew.cloudfront.net/";
-$_ENV['CLOUDFRONT_KEY_PAIR_ID']      =   "APKAIYR5D6P6UHWCD4RA";
+$_ENV['AWS_ACCESS_KEY']              = "";
+$_ENV['AWS_SECRET_KEY']              = "";
+$_ENV['S3_BUCKET_NAME']              =   "";
+$_ENV['CLOUDFRONT_URL']              =   "";
+$_ENV['CLOUDFRONT_KEY_PAIR_ID']      =   "";
 
-$_ENV['FCM_API_ACCESS_KEY'] = "AAAApfUc4s8:APA91bEFulDUlFf2jT7C33Op7p_F3HRPzvXA_NvlAwCkhGb8C6WrUZH_hnbaZyttzqv2xc6CDD4zmF3VFcax1df_XAjkK04uyD_QfozniPMumRrMdfDh4Jayn5OcK8kUFtRo-f3fLynL";
-$_ENV['FCM_SENDER_ID'] = "712781914831";
-$_ENV['FCM_WEB_API_KEY'] = "AIzaSyBR2uqp6rkaLCdcfycqc62hanukILTLyYU";
-$_ENV['FCM_LEGACY_KEY'] = "AIzaSyA5w1M_6FnnisYaK-hR8X2siIDC7sNU9HM";
+$_ENV['FCM_API_ACCESS_KEY'] = "";
+$_ENV['FCM_SENDER_ID'] = "";
+$_ENV['FCM_WEB_API_KEY'] = "";
+$_ENV['FCM_LEGACY_KEY'] = "";
 
 // AFRICA'S TALKING
-$_ENV['AFRICAS_TALKING_API_KEY'] = "a9a9b69935483beba293f312514a90d7f8681950d7d79e924e028f56b64e6605";
-$_ENV['AFRICAS_TALKING_USERNAME'] = "gmoney";
-$_ENV['AFRICAS_TALKING_SENDER_ID'] = "G-MONEY";
+$_ENV['AFRICAS_TALKING_API_KEY'] = "";
+$_ENV['AFRICAS_TALKING_USERNAME'] = "";
+$_ENV['AFRICAS_TALKING_SENDER_ID'] = "";
