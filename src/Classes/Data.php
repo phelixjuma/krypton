676245fed0aad6ad59069dbbaaad2bb15612e149
Phelix <phelix@Phelixs-MacBook-Pro.local>
2022-06-24 14:34:53 +0300

    formatting number; old

--- src/Classes/Data.php
+++ src/Classes/Data.php
@@ -220,6 +220,25 @@ final class Data {
 //        return $countryCode . $phoneNumber;
     }
 
+    public static function formatPhoneNumberOld($phoneNumber, $countryCode = "254") {
+
+        // Check if starts with '+'
+        $phoneNumber =  preg_replace('/\s+/', '', str_replace("+", "", $phoneNumber));
+
+        //check if the first 3 characters are the same as the country code.
+        if (substr($phoneNumber, 0, 3) == $countryCode) {
+            return $phoneNumber;
+        }
+
+        //check if the phone number starts with a 0.
+        if (substr($phoneNumber, 0, 1) == "0") {
+            //replace the zero with the country code
+            return substr_replace($phoneNumber, $countryCode, 0, 1);
+        }
+        // prepend 254
+        return $countryCode . $phoneNumber;
+    }
+
     /**
      * Format username
      * @param string $username
