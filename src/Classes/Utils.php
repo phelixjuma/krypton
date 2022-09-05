e68eeb68bc59f222517c6029c99f67f275a668f1
Phelix <phelix@Phelixs-MacBook-Pro.local>
2022-09-05 11:44:42 +0300

    remove emoji in escape

--- src/Classes/Utils.php
+++ src/Classes/Utils.php
@@ -54,8 +54,8 @@ final class Utils {
 
     /**
      * Escape the given string
-     * @param $input string to be escaped
-     * @return string escaped string
+     * @param $input
+     * @return array|string|string[]|null
      */
     public static function escape($input) {
         if(is_array($input)){
@@ -64,7 +64,7 @@ final class Utils {
             });
             return $input;
         }
-        return htmlspecialchars(urldecode(trim($input)), ENT_QUOTES);
+        return self::remove_emoji(htmlspecialchars(urldecode(trim($input)), ENT_QUOTES));
     }
 
     /**
@@ -82,7 +82,44 @@ final class Utils {
      * @return string
      */
     public static function full_unescape($string) {
-        return html_entity_decode(htmlspecialchars_decode($string, ENT_QUOTES), ENT_QUOTES);
+        return self::remove_emoji(html_entity_decode(htmlspecialchars_decode($string, ENT_QUOTES), ENT_QUOTES));
+    }
+
+    /**
+     * @param $string
+     * @return array|string|string[]|null
+     */
+    public static function remove_emoji($string) {
+
+        // Match Enclosed Alphanumeric Supplement
+        $regex_alphanumeric = '/[\x{1F100}-\x{1F1FF}]/u';
+        $clear_string = preg_replace($regex_alphanumeric, '', $string);
+
+        // Match Miscellaneous Symbols and Pictographs
+        $regex_symbols = '/[\x{1F300}-\x{1F5FF}]/u';
+        $clear_string = preg_replace($regex_symbols, '', $clear_string);
+
+        // Match Emoticons
+        $regex_emoticons = '/[\x{1F600}-\x{1F64F}]/u';
+        $clear_string = preg_replace($regex_emoticons, '', $clear_string);
+
+        // Match Transport And Map Symbols
+        $regex_transport = '/[\x{1F680}-\x{1F6FF}]/u';
+        $clear_string = preg_replace($regex_transport, '', $clear_string);
+
+        // Match Supplemental Symbols and Pictographs
+        $regex_supplemental = '/[\x{1F900}-\x{1F9FF}]/u';
+        $clear_string = preg_replace($regex_supplemental, '', $clear_string);
+
+        // Match Miscellaneous Symbols
+        $regex_misc = '/[\x{2600}-\x{26FF}]/u';
+        $clear_string = preg_replace($regex_misc, '', $clear_string);
+
+        // Match Dingbats
+        $regex_dingbats = '/[\x{2700}-\x{27BF}]/u';
+        $clear_string = preg_replace($regex_dingbats, '', $clear_string);
+
+        return $clear_string;
     }
 
     /**
